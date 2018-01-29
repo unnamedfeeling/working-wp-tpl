ce801956f471d14f11eae54b0765b0da40fa3f39
Alexandr <38067796687@yandex.ua>
2018-01-29 16:44:48 +0200

    updated function name in header

--- header.php
+++ header.php
@@ -12,7 +12,7 @@
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta name="description" content="<?php bloginfo('description'); ?>">
-		<?php wp_head(); 
+		<?php wp_head();
 		if(!empty($options['advd']['custom_css'])){
 			echo ($options['advd']['custom_css']!==' ') ? '<style>'.htmlspecialchars_decode($options['advd']['custom_css']).'</style>' : null;
 		}
@@ -27,6 +27,6 @@
 					</a>
 				</div>
 				<nav class="nav" role="navigation">
-					<?php html5blank_nav(); ?>
+					<?php generic_nav(); ?>
 				</nav>
 			</header>
